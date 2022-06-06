aaab395d0936306bee732e9d823a876b6ae67651
Andrey Avtomonov <andreybavt@gmail.com>
2022-06-07 01:58:21 +0200

    global API error handling

--- giskard/giskard_client.py
+++ giskard/giskard_client.py
@@ -2,14 +2,37 @@
 from typing import List
 from urllib.parse import urljoin
 
+from requests.adapters import HTTPAdapter
 from requests_toolbelt import sessions
 
 from giskard.project import GiskardProject
 
 
+class GiskardError(Exception):
+    pass
+
+
+class ErrorHandlingAdapter(HTTPAdapter):
+    def build_response(self, req, resp):
+        response = super().build_response(req, resp)
+
+        if not response.ok:
+            giskard_error = None
+            try:
+                err_resp = response.json()
+                giskard_error = GiskardError(
+                    f"{err_resp.get('title', 'Unknown error')}: {err_resp.get('detail', 'no details')}")
+            except:  # NOSONAR
+                response.raise_for_status()
+            raise giskard_error
+        return response
+
+
 class GiskardClient:
     def __init__(self, url: str, token: str):
-        self._session = sessions.BaseUrlSession(base_url=urljoin(url, "/api/v2/"))
+        base_url = urljoin(url, "/api/v2/")
+        self._session = sessions.BaseUrlSession(base_url=base_url)
+        self._session.mount(base_url, ErrorHandlingAdapter())
         self._session.headers.update({'Authorization': f"Bearer {token}"})
 
     @property
