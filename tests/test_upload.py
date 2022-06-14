51fa3e21a219b93850c6154c60a776413dc2583e
princyiakov <princy@giskard.ai>
2022-06-14 10:19:55 +0200

    GSK-123 classification unit test added

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -51,7 +51,7 @@ def test_upload_df(diabetes_dataset):
 
 
 @httpretty.activate(verbose=True, allow_net_connect=False)
-def test_upload_model(linear_regression_diabetes: GiskardModel, diabetes_dataset):
+def test_upload_regression_model(linear_regression_diabetes: GiskardModel, diabetes_dataset):
     httpretty.register_uri(
         httpretty.POST,
         "http://giskard-host:12345/api/v2/project/models/upload"
@@ -91,3 +91,45 @@ def test_upload_model(linear_regression_diabetes: GiskardModel, diabetes_dataset
     assert requirements_file.content.decode()
     # assert re.search(r'scikit-learn==[\d\\.]+', requirements_file.content.decode())
     # assert re.search(r'pandas==[\d\\.]+', requirements_file.content.decode())
+
+
+@httpretty.activate(verbose=True, allow_net_connect=False)
+def test_upload_classification_model(german_credit_model: GiskardModel, german_credit_data):
+    httpretty.register_uri(
+        httpretty.POST,
+        "http://giskard-host:12345/api/v2/project/models/upload"
+    )
+    df, input_types, target = german_credit_data
+
+    model = german_credit_model
+    client = GiskardClient("http://giskard-host:12345", "SECRET_TOKEN")
+    project = GiskardProject(client.session, "test-project")
+    project.upload_model(
+        prediction_function=model.prediction_function,
+        model_type=model.model_type,
+        feature_names=model.feature_names,
+        name="uploaded model",
+        validate_df=df,
+        classification_labels=model.classification_labels
+    )
+    with pytest.raises(Exception):
+        project.upload_model(
+            prediction_function=model.prediction_function,
+            model_type=model.model_type,
+            feature_names=input_types,
+            name="uploaded model",
+            validate_df=df
+        )
+    req = httpretty.last_request()
+    assert req.headers.get('Authorization') == 'Bearer SECRET_TOKEN'
+    assert int(req.headers.get('Content-Length')) > 0
+    assert req.headers.get('Content-Type').startswith('multipart/form-data; boundary=')
+
+    multipart_data = decoder.MultipartDecoder(req.body, req.headers.get('Content-Type'))
+    assert len(multipart_data.parts) == 3
+    meta, model_file, requirements_file = multipart_data.parts
+    assert meta.headers.get(b'Content-Type') == b'application/json'
+    loaded_model = load_decompress(model_file.content)
+
+    assert np.array_equal(loaded_model(df), model.prediction_function(df))
+    assert requirements_file.content.decode()
