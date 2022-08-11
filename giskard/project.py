103b12eed431c88a69e6974515ef65f0d52b4f5f
princyiakov <princy@giskard.ai>
2022-08-11 15:43:33 +0200

    GSK-239 better error message

--- giskard/project.py
+++ giskard/project.py
@@ -378,8 +378,8 @@ class GiskardProject:
 
     @staticmethod
     def _verify_prediction_output(df: pd.DataFrame,model_type, prediction):
-        assert len(df) == len(prediction), f"Row length '{len(df)}' of dataframe provided does not match with " \
-                                           f"Output length '{len(prediction)}' of prediction_function"
+        assert len(df) == len(prediction), f"Number of rows of dataset provided({len(df)}) does not match with the " \
+                                           f"number of rows prediction_function output({len(prediction)})"
         if isinstance(prediction, np.ndarray) or isinstance(prediction, list):
             if model_type == SupportedModelTypes.CLASSIFICATION.value:
                 if not any(isinstance(y, (np.floating, float)) for x in prediction for y in x):
