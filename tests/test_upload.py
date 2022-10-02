2c521a0a82817e8efff9a42d9b2c27d101f227f1
princyiakov <princy@giskard.ai>
2022-10-02 10:37:58 +0200

    GSK-301 Added additional unit tests, Refactored test_upload.py to segregate default and exception scenario

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -85,14 +85,6 @@ def _test_upload_model(model: GiskardModel, data):
             classification_labels=model.classification_labels
         )
 
-    with pytest.raises(Exception):
-        project.upload_model(
-            prediction_function=model.prediction_function,
-            model_type=model.model_type,
-            feature_names=input_types,
-            name=model_name,
-            validate_df=df
-        )
     req = httpretty.last_request()
     assert req.headers.get('Authorization') == auth
     assert int(req.headers.get('Content-Length')) > 0
@@ -105,6 +97,33 @@ def _test_upload_model(model: GiskardModel, data):
     if model.model_type == 'classification':
         metadata = json.loads(meta.content)
         assert np.array_equal(model.classification_labels, metadata.get('classificationLabels'))
+
+    assert meta.headers.get(b'Content-Type') == b_content_type
+    loaded_model = load_decompress(model_file.content)
+
+    assert np.array_equal(loaded_model(df), model.prediction_function(df))
+    assert requirements_file.content.decode()
+
+
+def _test_upload_model_exceptions(model: GiskardModel, data):
+    df, input_types, target = data
+
+    client = GiskardClient(url, token)
+    project = GiskardProject(client.session, "test-project")
+
+    # Error Scenario : Column_types dictionary sent as feature_names
+    with pytest.raises(Exception):
+        project.upload_model(
+            prediction_function=model.prediction_function,
+            model_type=model.model_type,
+            feature_names=input_types,
+            name=model_name,
+            validate_df=df,
+            classification_labels=model.classification_labels
+        )
+
+    if model.model_type == 'classification':
+        # Error Scenario: Classification model sent without classification_labels
         with pytest.raises(Exception):
             project.upload_model_and_df(
                 prediction_function=model.prediction_function,
@@ -112,14 +131,21 @@ def _test_upload_model(model: GiskardModel, data):
                 df=df,
                 column_types=input_types,
                 feature_names=model.feature_names,
-                model_name=model_name,
-                classification_labels=model.classification_labels
+                model_name=model_name
             )
-    assert meta.headers.get(b'Content-Type') == b_content_type
-    loaded_model = load_decompress(model_file.content)
 
-    assert np.array_equal(loaded_model(df), model.prediction_function(df))
-    assert requirements_file.content.decode()
+        # Error Scenario: Target has values not declared in Classification Label
+        with pytest.raises(Exception):
+            project.upload_model_and_df(
+                prediction_function=model.prediction_function,
+                model_type=model.model_type,
+                target='default',
+                df=df,
+                column_types=input_types,
+                feature_names=model.feature_names,
+                model_name=model_name,
+                classification_labels=[0, 1]
+            )
 
 
 @pytest.mark.parametrize('data,model,',
@@ -129,3 +155,12 @@ def test_upload_models(data, model, request):
     data = request.getfixturevalue(data)
     model = request.getfixturevalue(model)
     _test_upload_model(model, data)
+
+
+@pytest.mark.parametrize('data,model,',
+                         [('german_credit_data', 'german_credit_model'),
+                          ('diabetes_dataset', 'linear_regression_diabetes')])
+def test_upload_models_exceptions(data, model, request):
+    data = request.getfixturevalue(data)
+    model = request.getfixturevalue(model)
+    _test_upload_model_exceptions(model, data)
