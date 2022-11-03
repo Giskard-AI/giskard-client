b9b8176abe9571defa257e965f8f225534c981ed
andreybavt <andreybavt@gmail.com>
2022-11-03 18:45:48 +0100

    added install dev client script

--- scripts/install-giskard-client-dev.sh
+++ scripts/install-giskard-client-dev.sh
@@ -1,4 +1,4 @@
-pip="${0:-pip}"
+pip="${1:-pip}"
 rm -f /tmp/giskard*whl* || true
 wget https://nightly.link/Giskard-AI/giskard-client/workflows/install-test/main/giskard-dev-3.7.whl.zip -P /tmp
 unzip -o /tmp/giskard-dev-3.7.whl.zip -d /tmp
