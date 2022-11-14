5c283383bc4b211eae523b215f3a52a774929b78
andreybavt <andreybavt@gmail.com>
2022-11-14 17:19:17 +0100

    GSK-354 LIME doesn't work

--- tests/test_model_explanation.py
+++ tests/test_model_explanation.py
@@ -3,7 +3,7 @@ import pytest
 
 from giskard.ml_worker.core.giskard_dataset import GiskardDataset
 from giskard.ml_worker.core.model import GiskardModel
-from giskard.ml_worker.core.model_explanation import explain
+from giskard.ml_worker.core.model_explanation import explain, explain_text
 
 
 @pytest.mark.parametrize(
@@ -17,6 +17,7 @@ from giskard.ml_worker.core.model_explanation import explain
         ("german_credit_data", "german_credit_model", False),
         ("diabetes_dataset", "linear_regression_diabetes", False),
         ("diabetes_dataset_with_target", "linear_regression_diabetes", False),
+        ("enron_data", "enron_model", False),
     ],
 )
 def test_explain(ds_name: str, model_name: str, include_feature_names: bool, request):
@@ -42,3 +43,11 @@ def test_explain(ds_name: str, model_name: str, include_feature_names: bool, req
         exp = explanations.get("explanations").get("default")
         for l, e in exp.items():
             assert np.issubdtype(type(e), np.floating), f"'{l}' explanation value isn't float"
+
+
+def test_explain_text(enron_test_data, enron_model):
+    df = enron_test_data.df.dropna()
+    sample = df.head(1)
+    result = explain_text(model=enron_model, input_df=sample, text_column='Content',
+                          text_document=sample['Content'].iloc[0], n_samples=10)
+    assert result
